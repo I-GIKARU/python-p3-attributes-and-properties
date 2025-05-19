ea0c996df0fb19b0256e813b2de72d9195214f63
User <user@example.com>
2025-05-19 11:02:03 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Mutt"):
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+        if breed not in APPROVED_BREEDS and breed != "Mutt":
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
